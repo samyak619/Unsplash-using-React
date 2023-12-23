ec22808d2d9805a1d2b512b795d8d7f569a0b05f
samyak619 <samyakhadse@gmail.com>
2023-12-23 13:10:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 function App() {
   const [value, setValue] = useState("");
   const [results, setResults] = useState([]);
+
   const fetchImages = () => {
     fetch(
       `https://api.unsplash.com/search/photos/?client_id=_-3c9DmqjLkglthRIse_XRCzccJVHVOaYmPWPkf6KRw&query=${value}&per_page=40`
@@ -15,6 +16,26 @@ function App() {
       });
   };
 
+  const handleDownload = async (imageUrl, index) => {
+    try {
+      // Fetch the image as a blob
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+
+      // Save the image blob to local storage
+      const fileName = `image_${index}.jpg`;
+      localStorage.setItem(fileName, JSON.stringify(blob));
+
+      // Create a link to trigger the download
+      const downloadLink = document.createElement("a");
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = fileName;
+      downloadLink.click();
+    } catch (error) {
+      console.error("Error downloading image:", error);
+    }
+  };
+
   return (
     <>
       <div className="container">
@@ -24,23 +45,25 @@ function App() {
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-        <button className="button" onClick={() => fetchImages()}>
+        <button className="button" onClick={fetchImages}>
           Send
         </button>
       </div>
-        {
-          <div className="images">
-            {results.map((item) => {
-              return (
-                <img src={item.urls.regular}  />
-              );
-            })}
+      <div className="images">
+        {results.map((item, index) => (
+          <div className="output" key={index}>
+            <img src={item.urls.regular} alt={`Images${index}`} />
+            <button
+              className="download-button"
+              onClick={() => handleDownload(item.urls.regular, index)}
+            >
+              Download
+            </button>
           </div>
-        }
+        ))}
+      </div>
     </>
   );
 }
 
 export default App;
-
-//_-3c9DmqjLkglthRIse_XRCzccJVHVOaYmPWPkf6KRw
